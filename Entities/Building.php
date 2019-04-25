1c487a89fa0802e01960eb25f60c1160b8973048
polina-okhai <polina.okhay@gmail.com>
2019-04-25 21:38:08 +0300

    Create simple class

---
+++ Entities/Building.php
@@ -0,0 +1,10 @@
+<?php
+class Building {
+  public $name = 'Polina';
+  public $surname = 'Okhai';
+
+  public function fullName() {
+    echo "$this->name $this->surname";
+  }
+}
+ ?>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+require 'Entities/Building.php';
+ini_set('display_errors', 1);
+
+$building = new Building;
+$building->fullName();
+ ?>
